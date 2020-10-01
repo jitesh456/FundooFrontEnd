3d6534e3647062511d06301acbf00c9b30fdacf5
jitesh dubey <jitesh0dubey@gmail.com>
2020-10-01 22:25:57 +0530

    [Jitesh Dubey]ADDED:validation for reset password

--- src/Component/ResetPassword.js
+++ src/Component/ResetPassword.js
@@ -1,6 +1,42 @@
-import React from 'react';
-import {InputGroup,FormControl,Card,Button} from 'react-bootstrap';
+import React,{useState} from 'react';
+import {InputGroup,FormControl,Card,Button,Form,FormGroup} from 'react-bootstrap';
 export default function ResetPassword(){
+
+    const passwordPattern=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@|.|!|#])(?=.*[a-zA-Z]).{8,}$/;
+    const [validated, setValidated] = useState(false);
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');    
+    const [passwordErrorStatus, setPasswordErrorStatus] = useState(true);
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
+    const [confirmPasswordErrorStatus, setConfirmPasswordErrorStatus] = useState(true);
+
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if(!passwordPattern.test(password)){
+          setPasswordError('Not a valid Password');
+          setPasswordErrorStatus(false);  
+          event.preventDefault();
+          event.stopPropagation();        
+      }
+      if(confirmPassword !== password || !passwordPattern.test(password) ){
+        setConfirmPasswordError('confirm password must be same as password');
+        setConfirmPasswordErrorStatus(false);  
+        event.preventDefault();
+        event.stopPropagation();        
+     }
+     if(passwordPattern.test(password)){
+        setPasswordError('');
+        setPasswordErrorStatus(true);                
+    }
+    if(confirmPassword === password && passwordPattern.test(password) ){
+      setConfirmPasswordError('');
+      setConfirmPasswordErrorStatus(true);  
+             
+   }
+      
+   
+    };
 
     return(
         <div className="form">
@@ -16,27 +52,40 @@ export default function ResetPassword(){
                     <p style={{fontSize:"16px"}}>Use Your Fundoo Account</p>
                     </div>
             </div>
-            <InputGroup className="mb-3">
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <InputGroup className="mb-3" controlId="validationCustom01">
+            
             <FormControl
             placeholder="Password"
             type="Password"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
+            onChange={e=>{setPassword(e.target.value)}}            
+            className={passwordErrorStatus ? "form-control " : "form-control is-invalid"}
+            required
             />
-    
+            <Form.Control.Feedback type="invalid">
+                {passwordError}
+            </Form.Control.Feedback>
             </InputGroup>
-            <InputGroup className="mb-3">
+            <InputGroup className="mb-3" controlId="validationCustom01">
             <FormControl
             placeholder="Confirm Password"
             type="Password"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
+            onChange={e=>{setConfirmPassword(e.target.value)}}    
+            className={confirmPasswordErrorStatus ? "form-control " : "form-control is-invalid"}     
+            required
             />
-    
+            <Form.Control.Feedback type="invalid">
+              {confirmPasswordError}
+            </Form.Control.Feedback>
             </InputGroup>  
             <div style={{display:"flex",justifyContent:"flex-end"}}>
-            <Button variant="primary">Next </Button>
+            <Button variant="primary" type="submit"> Next </Button>            
             </div>
+            </Form>
             </Card.Body>
             </Card>
            
